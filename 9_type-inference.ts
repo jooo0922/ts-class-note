1a66b01bc54f223a4fc821c6ffd44309635f6946
jooo0922 <2014102458@khu.ac.kr>
2022-04-06 00:01:30 +0900

    add: Learn complex type inference using generics

--- 9_type-inference.ts
+++ 9_type-inference.ts
@@ -11,12 +11,41 @@ function getB(b = 10) {
 }
 
 // 타입 추론 기본 2
+// interface Dropdown<T> {
+//   value: T;
+//   title: string;
+// }
+// generic 으로 전달한 타입을 통해 value 속성값의 타입까지 추론하는 것도 TS 랭귀지 서버의 타입 추론으로 보면 됨.
+// const shoppingItem: Dropdown<string> = {
+//   value: "abc",
+//   title: "hello",
+// };
+
+// 타입 추론 기본 3
 interface Dropdown<T> {
   value: T;
   title: string;
 }
-// generic 으로 전달한 타입을 통해 value 속성값의 타입까지 추론하는 것도 TS 랭귀지 서버의 타입 추론으로 보면 됨.
-const shoppingItem: Dropdown<string> = {
-  value: "abc",
-  title: "hello",
+// 원래 제네릭 타입 작성 시 관행적으로 <T> 라고 쓰지만, 여기서는 구분을 해주기 위해 <K> 로 씀.
+/**
+ * 여기서 K 에 제네릭을 넘겨주면,
+ * K는 일단 DetailedDropdown 인터페이스의 tag 속성값의 타입을 K로 지정할 것이고,
+ * DetailedDropdown 이 상속받는 Dropdown 인터페이스 또한 K 타입을 제네릭으로 받아서
+ * Dropdown 의 value 속성값 타입도 K로 지정됨. -> 이런 식으로 2개 이상의 제네릭을 사용하여 복잡한 타입 추론 과정을 거칠 수 있음.
+ *
+ * 또한, 인터페이스를 상속받기 때문에 DetailedDropdown 타입이 지정된
+ * 변수에는 value, title 값도 포함되어 있어야겠지.
+ */
+interface DetailedDropdown<K> extends Dropdown<K> {
+  descriptiom: string;
+  tag: K;
+  // value,
+  // title,
+}
+
+let detailedItem: DetailedDropdown<string> = {
+  title: "abc",
+  descriptiom: "ab",
+  value: "a",
+  tag: "a",
 };
